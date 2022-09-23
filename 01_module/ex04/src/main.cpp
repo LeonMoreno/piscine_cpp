2090879821dc97a275db6c815871d9072b5be9b3
Leonardo Moreno <lmoreno@student.42quebec.com>
2022-09-22 21:01:25 -0400

    02_module - ex04 / Finito

--- 01_module/ex04/src/main.cpp
+++ 01_module/ex04/src/main.cpp
@@ -10,6 +10,28 @@ void	ft_Files(std::string &replace, std::ifstream &s, char *argv1)
 	replace = replace + ".replace";
 }
 
+void	searchReplace(char **argv, std::string & str)
+{
+	std::string search = argv[2];
+	std::string replace = argv[3];
+
+	size_t start_pos = 0;
+	while ((start_pos = str.find(search, start_pos)) != std::string::npos)
+	{
+		str.erase(start_pos, search.length());
+		str.insert(start_pos, replace);
+		// std::cout << str << std::endl;
+		// std::cout << "Encontrado en : " << start_pos << std::endl;
+		start_pos += search.length();
+	}
+
+	//  std::cout << "Ensasyo " <<  search.insert(1, replace) << std::endl;
+	// std::cout << "Ensasyo " <<  search + replace << std::endl;
+	// std::size_t found = str.find(search);
+	// if (found != std::string::npos)
+		// std::cout << "Encontrado en : " << found << std::endl;
+}
+
 void	ft_startSed(char **argv)
 {
 	std::ifstream	ifs;
@@ -22,10 +44,13 @@ void	ft_startSed(char **argv)
 	std::getline(ifs, str);
 	while (ifs.good())
 	{
+		searchReplace(argv, str);
 		ofs << str << std::endl;
-		std::cout << str << std::endl;
+	//	std::cout << str << std::endl;
 		std::getline(ifs, str);
 	}
+	ifs.close();
+	ofs.close();
 }
 
 int	main(int argc, char **argv)
