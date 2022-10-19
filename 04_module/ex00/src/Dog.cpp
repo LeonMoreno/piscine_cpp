dc19bd1acad953872f25cd54930ef9c0d261a67c
Leonardo Moreno <lmoreno@student.42quebec.com>
2022-10-19 11:58:53 -0400

    🔨 04_mod - ex00 done

--- 04_module/ex00/src/Dog.cpp
+++ 04_module/ex00/src/Dog.cpp
@@ -5,6 +5,17 @@ Dog::Dog( void ) {
 	std::cout <<  YEL "Dog :" << RESET << "Default Constructor" << this << std::endl;
 }
 
+Dog::Dog(Dog const & src) {
+	std::cout << GRN "Dog: " RESET " COPY Constructor" << this << std::endl;
+	this->type = src.type;
+}
+
+Dog & Dog::operator=(Dog const & d) {
+	std::cout << "Dog: Asignation Constructor " << this  << std::endl;
+	this->type = d.type;
+	return (*this);
+}
+
 Dog::~Dog( void ) {
 	std::cout << "Dog : Default Destructor" << std::endl;
 }
--- 04_module/ex00/src/WrongCat.cpp
+++ 04_module/ex00/src/WrongCat.cpp
@@ -7,6 +7,17 @@ WrongCat::WrongCat( void ) {
 	std::cout << CYN "WrongCat :" << RESET <<  "Default Constructor"  << this << std::endl;
 }
 
+WrongCat::WrongCat(WrongCat const & src) {
+	std::cout << GRN "WrongCat: " RESET " COPY Constructor" << this << std::endl;
+	this->type = src.type;
+}
+
+WrongCat & WrongCat::operator=(WrongCat const & d) {
+	std::cout << "WrongCat: Asignation Constructor " << this  << std::endl;
+	this->type = d.type;
+	return (*this);
+}
+
 WrongCat::~WrongCat( void ) {
 	std::cout << "WrongCat : Default Destructor" << std::endl;
 }
