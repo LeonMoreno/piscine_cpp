2df05b1f5d0c881852af8e9b7a9c504560eb6024
Leonardo Moreno <lmoreno@student.42quebec.com>
2022-10-19 11:50:01 -0400

    🐛 04_mod - ex03 canonical class in cpp

--- 04_module/ex00/src/Cat.cpp
+++ 04_module/ex00/src/Cat.cpp
@@ -2,11 +2,22 @@
 
 Cat::Cat( void ) {
 	this->type = "Cat";
-	std::cout << GRN "Cat :" RESET "Default Constructor" << this << std::endl;
+	std::cout << GRN "Cat: " RESET "Default Constructor" << this << std::endl;
+}
+
+Cat::Cat(Cat const & src) {
+	std::cout << GRN "Cat: " RESET " COPY Constructor" << this << std::endl;
+	this->type = src.type;
+}
+
+Cat & Cat::operator=(Cat const & d) {
+	std::cout << "Cat: Asignation Constructor " << this  << std::endl;
+	this->type = d.type;
+	return (*this);
 }
 
 Cat::~Cat( void ) {
-	std::cout << "Cat : Default Destructor" << std::endl;
+	std::cout << GRN "Cat: " RESET " DesTRUCTOR " << this << std::endl;
 }
 
 void Cat::makeSound( void ) {
