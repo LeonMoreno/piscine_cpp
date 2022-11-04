46dc9a067f03127bd8c7420a335ec33e4f1d3f7d
Leo <leon.moreno@pm.me>
2022-11-03 21:49:05 -0400

    07_mod -- ex01 done

--- 07_module/ex01/include/Iter.hpp
+++ 07_module/ex01/include/Iter.hpp
@@ -14,13 +14,24 @@ void	iter(T *array, int len, void (*f)(T &))
 template <typename T>
 void	ftPrint(T argu)
 {
-	std::cout << argu << std::endl;
+	std::cout << std::fixed;
+	std::cout.precision(1);
+	std::cout << argu << " ";
 }
 
 template <typename T>
 void	ftIncrement(T argu)
 {
 	argu += 10;
+}
+
+template <typename T>
+void	bubbleSort(T *arr, int len)
+{
+	for (int i = 0; i < (len - 1); i++)
+		for (int j = 0; j < (len - i - 1); j++)
+			if (arr[j] > arr[j + 1])
+				std::swap(arr[j], arr[j + 1]);
 }
 
 #endif
--- 07_module/ex01/src/main.cpp
+++ 07_module/ex01/src/main.cpp
@@ -2,36 +2,63 @@
 
 int	main(void)
 {
+	std::cout << "\n\t int template "  << std::endl;
 	{
-		int	array[] = {54, 32, 53, 2, 7};
+		int	arr[] = {43, 54, 32, 534, 4, 109, 59};
+		int len = sizeof(arr) / sizeof(int);
 
-		std::cout << "Tes1" << std::endl;
-		iter(array, 5, &ftPrint);
-		iter(array, 5, &ftIncrement);
+		std::cout << "Print_1 int : ";
+		iter(arr, len, &ftPrint); // Implicit conversion
+		std::cout << std::endl;
+		iter<int>(arr, len, &ftIncrement); // Explicit conversion
 
-		std::cout << "\nTes2" << std::endl;
-		iter(array, 5, &ftPrint);
+		std::cout << "Print_2 int : ";
+		iter<int>(arr, len, &ftPrint);
+		std::cout << std::endl;
+
+		bubbleSort<int>(arr, len);
+		std::cout << "Print_3 int : ";
+		iter(arr, len, &ftPrint);
+		std::cout << std::endl;
+	}
+	std::cout << "\n\t Float template "  << std::endl;
+	{
+		float	arr[] = {3.54f, 32.3f, 90.4f, 534.32f, 4, 109.232f, 59};
+		int len = sizeof(arr) / sizeof(float);
+
+		std::cout << "Print_1 int : ";
+		iter(arr, len, &ftPrint); // Implicit conversion
+		std::cout << std::endl;
+		iter<float>(arr, len, &ftIncrement); // Explicit conversion
+
+		std::cout << "Print_2 int : ";
+		iter<float>(arr, len, &ftPrint);
+		std::cout << std::endl;
+
+		bubbleSort<float>(arr, len);
+		std::cout << "Print_3 int : ";
+		iter(arr, len, &ftPrint);
+		std::cout << std::endl;
+	}
+	std::cout << "\n\t char template "  << std::endl;
+	{
+		char	arr[] = {'d', 'r', '4', 'P', '0', 'Q'};
+		int len = sizeof(arr) / sizeof(char);
+
+		std::cout << "Print_1 int : ";
+		iter(arr, len, &ftPrint); // Implicit conversion
+		std::cout << std::endl;
+		iter<char>(arr, len, &ftIncrement); // Explicit conversion
+
+		std::cout << "Print_2 int : ";
+		iter<char>(arr, len, &ftPrint);
+		std::cout << std::endl;
+
+		bubbleSort<char>(arr, len);
+		std::cout << "Print_3 int : ";
+		iter(arr, len, &ftPrint);
+		std::cout << std::endl;
 	}
 
-	{
-		float	array[] = {54.87, 32.87, 53.43, 2, 7.33};
-
-		std::cout << "\n Tes1 Float" << std::endl;
-		iter(array, 5, &ftPrint);
-		iter(array, 5, &ftIncrement);
-
-		std::cout << "\nTes2" << std::endl;
-		iter(array, 5, &ftPrint);
-	}
-
-	{
-		char	array[] = {'f', 'd', 'Z', 'g', 'j'};
-
-		std::cout << "\n Tes1 Float" << std::endl;
-		iter(array, 5, &ftPrint);
-		iter(array, 5, &ftIncrement);
-
-		std::cout << "\nTes2" << std::endl;
-		iter(array, 5, &ftPrint);
-	}
 }
+
